ac3d25dd42d2a17ee7fdb50f8961d5135e9752dc
Boismain Florian <boismain.florian@gmail.com>
2023-04-26 14:36:10 +0200

    question 1 and 2

--- js/training.js
+++ js/training.js
@@ -1,9 +1,21 @@
 /* ------------------------------------ */
 /* --- Exercice 1 --- */
-
-
+const button = document.querySelector("#first-button");
+let textInvis = document.querySelector("#test")
+button.addEventListener('click', addInvis);
+// console.log(textInvis);
+function addInvis(text){
+    textInvis.classList.toggle('invisibilities');
+}
+// console.log(addInvis( text-invis));
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
+const value = document.getElementById("ex2-scroll-value");
+
+window.addEventListener('scroll', function(){
+    value.textContent = window.scrollY;
+}) 
+// .innerHeight
 
 
 /* ------------------------------------ */
