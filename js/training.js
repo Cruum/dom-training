f24c8d55837939d58f2bed9e9661881c38e8dc9c
Boismain Florian <boismain.florian@gmail.com>
2023-04-26 16:54:30 +0200

    exo5

--- js/training.js
+++ js/training.js
@@ -4,7 +4,7 @@ const button = document.querySelector("#first-button");
 let textInvis = document.querySelector("#test")
 button.addEventListener('click', addInvis);
 // console.log(textInvis);
-function addInvis(text){
+function addInvis(text) {
     textInvis.classList.toggle('invisibilities');
 }
 // console.log(addInvis( text-invis));
@@ -12,17 +12,17 @@ function addInvis(text){
 /* --- Exercice 2 --- */
 const value = document.getElementById("ex2-scroll-value");
 
-window.addEventListener('scroll', function(){
-    value.textContent = Math.round( window.scrollY );
-}) 
+window.addEventListener('scroll', function () {
+    value.textContent = Math.round(window.scrollY);
+})
 
 
 /* ------------------------------------ */
 /* --- Exercice 3 --- */
 
-const list =  document.getElementById('ex3-animals');
-const buttonAnimals=document.getElementById("buttonAnimals");
-buttonAnimals.addEventListener('click', function() {
+const list = document.getElementById('ex3-animals');
+const buttonAnimals = document.getElementById("buttonAnimals");
+buttonAnimals.addEventListener('click', function () {
     // const firstchild = list.firstElementChild;
     list.appendChild(list.firstElementChild);
 });
@@ -32,28 +32,76 @@ buttonAnimals.addEventListener('click', function() {
 /* --- Exercice 4 --- */
 const buttonColor = document.querySelector('#buttonColor');
 
-const color = ["red", "blue","green"];
+const color = ["red", "blue", "green"];
 let indexColors = 0;
 
-// buttonColor.addEventListener("click", changerCouleur);
-//       function changerCouleur() {
-//         textbackgroundcolor.style.color = color;
-// }
-
-
-buttonColor.addEventListener('click', function() {
-    this.style.background = color[indexColors%color.length] ;
-indexColors++;
-  });
+buttonColor.addEventListener('click', function () {
+    this.style.background = color[indexColors % color.length];
+    indexColors++;
+});
 
 /* ------------------------------------ */
 /* --- Exercice 5 --- */
 
+// const getRandomColor = () => {
+const buttons = document.querySelectorAll("#ex5 .button");
+buttons.forEach((button) => {
+    button.addEventListener("mouseover", function () {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        button.style.backgroundColor = "#" + randomColor;
+    });
+});
 
 /* ------------------------------------ */
 /* --- Exercice 6 --- */
 
+const LetterContining = document.querySelector("#ex6-paragraph");
+// const allLetter = LetterContining.split(" ");
+console.log(LetterContining);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener("DOMContentLoaded", (event) => {
+//     animate_text();
+//   });
+//   // -------------------
+//   function animate_text() 
+//   {
+//     let delay = 100,
+//         delay_start = 0,
+//         contents,
+//         letters;
+  
+//     document.querySelectorAll(".animate-text").forEach(function (elem) {
+//       contents = elem.textContent.trim();
+//       elem.textContent = "";
+//       letters = contents.split("");
+//       elem.style.visibility = 'visible';
+  
+//       letters.forEach(function (letter, index_1) {
+//         setTimeout(function () {
+//           // ---------
+//           // effet machine à écrire (SIMPLE)
+//           elem.textContent += letter;
+
+//         }, delay_start + delay * index_1);
+//       });    
+//       delay_start += delay * letters.length;
+//     });
+//   }
 
 /* ------------------------------------ */
 /* --- Exercice 7 --- */
